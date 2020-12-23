f33bd5768f425b97dee0541b41cd8b0157780142
Manuel E Mendez <emilio_mem@hotmail.com>
2020-12-23 13:01:41 -0400

    Add project files.

---
+++ MessageCodePin/MessageCodePin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MessageCodePin.Models;
+
+namespace MessageCodePin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
